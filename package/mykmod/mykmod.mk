2eaddc36bb93d9d0661b01cfdf6342ed2b38e886
Kirill Mirkin <kmirkin@luxoft.com>
2016-11-05 17:12:49 +0200

    Added missing files, renamed package

--- package/mykmod/mykmod.mk
+++ package/mykmod/mykmod.mk
@@ -1,6 +1,5 @@
 MYKMOD_VERSION = 1.0
 MYKMOD_SOURCE = mykmod-$(MYKMOD_VERSION).tar.bz2
-MYKMOD_SITE = http://somewhere.com/
 
 MYKMOD_DEPENDENCIES = linux
 
@@ -12,6 +11,5 @@ define MYKMOD_INSTALL_TARGET_CMDS
 	$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) M=$(@D) modules_install
 endef
 
-#$(eval $(call GENTARGETS,package,mykmod))
 $(eval $(kernel-module))
 $(eval $(generic-package))
